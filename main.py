a07192f504356c3587559412971a0d5266c40bf2
Jurij Procenko <elsoul@gmail.com>
2023-08-12 16:44:36 +0300

    make MVP with all fuctions

--- main.py
+++ main.py
@@ -7,19 +7,14 @@ images, documents, video, audio, archives
 
 from sys import argv
 import os
+from pathlib import Path
 import string
-
-
-# Check folder for empty and delete
-def isempty(folder: str):
-    if not os.listdir(folder):
-        os.rmdir(folder)
 
 
 # Rename file name from cyrillic to latin
 def normalize(cyrillic_name: str) -> str:
-    ext = cyrillic_name[cyrillic_name.find(".") + 1 :]
-    cyrillic_name = cyrillic_name[: cyrillic_name.find(".")]
+    ext = cyrillic_name[cyrillic_name.rfind(".") + 1 :]
+    cyrillic_name = cyrillic_name[: cyrillic_name.rfind(".")]
     name_out = ""
     # Convert cyrilic letters to latin, latin letters and
     # numbers remain as they are, other characters are replaced by the _ character
@@ -36,15 +31,69 @@ def normalize(cyrillic_name: str) -> str:
     return name_out + "." + ext
 
 
-# Sort and move files from current folder to destination
-# If there are subfolders call himself for these folders
-def sort_and_move(folder: str):
-    pass
+def rename_all_files():
+    for key in dest_folders:
+        # home = Path(dest_folders[key]+'\\')
+        os.chdir(dest_folders[key])
+        for next_file in Path.cwd().glob("*.*"):
+            name = str(next_file.name)
+            next_file.replace(normalize(name))
+
+
+def make_dir():
+    for key in dest_folders:
+        try:
+            os.mkdir(dest_folders[key])
+        except:
+            print(f"Folder {key} is alredy exists")
+
+
+def move_files():
+    for key in extentions:
+        for ext in extentions[key]:
+            for next_file in home.glob(f"**/*.{ext}"):
+                name = next_file.name
+                next_file = str(next_file)
+                known_ext.add(next_file[next_file.rfind(".") + 1 :].lower())
+                try:
+                    os.rename(next_file, dest_folders[key] + "\\" + name)
+                except:
+                    # os.remove(next_file)
+                    print(f"File {next_file} exists in destination folder")
+
+
+def find_unknown_ext():
+    # Собираем неизвестные расширения в множество
+    for next_file in home.glob(f"**/*.*"):
+        next_file = str(next_file)
+        if next_file[next_file.rfind(".") + 1 :].lower() not in known_ext:
+            unknown_ext.add(next_file[next_file.rfind(".") + 1 :].lower())
+
+
+def remove_empty_folders():
+    for next_file in home.glob(f"**/*"):
+        if next_file.is_dir() and len(os.listdir(next_file)) == 0:
+            next_file.rmdir()
+
+
+def unpack_archives():
+    for next_file in Path(dest_folders["archives"]).glob("*.*"):
+        name = str(next_file)
+        dest = name[: name.rfind(".")] + "\\"
+        os.mkdir(dest)
+        zip_command = f"tar -xvzf {name} -C {dest}"
+        try:
+            print(name)
+            os.system(zip_command)
+        except:
+            print(f"Something wrong with file {name}")
+        next_file.unlink()
 
 
 # Taking an argument from command line
 # source_folder = argv[1]
 source_folder = "w:\\Projects\\HW1\\1"
+home = Path(source_folder)
 
 # Cyrillic and latin
 cyr_lat = {
@@ -83,41 +132,51 @@ cyr_lat = {
     "я": "ja",
 }
 
-# Iniatializing variables
-img = ["JPEG", "PNG", "JPG", "SVG"]
-doc = ["DOC", "DOCX", "TXT", "PDF", "XLSX", "PPTX"]
-video = ["AVI", "MP4", "MOV", "MKV"]
-audio = ["MP3", "OGG", "WAV", "AMR"]
-archives = ["ZIP", "GZ", "TAR"]
-
 # Destination folders
-img_folder = source_folder + "images"
-doc_folder = source_folder + "documents"
-video_folder = source_folder + "video"
-audio_folder = source_folder + "audio"
-archives_folder = source_folder + "archives"
-
-# Lists for known and unknown extentions
+dest_folders = {
+    "image": source_folder + "\\images",
+    "doc": source_folder + "\\documents",
+    "video": source_folder + "\\video",
+    "audio": source_folder + "\\audio",
+    "archives": source_folder + "\\archives",
+}
+extentions = {
+    "image": ["JPEG", "PNG", "JPG", "SVG"],
+    "doc": ["DOC", "DOCX", "TXT", "PDF", "XLSX", "PPTX"],
+    "video": ["AVI", "MP4", "MOV", "MKV"],
+    "audio": ["MP3", "OGG", "WAV", "AMR"],
+    "archives": ["ZIP", "GZ", "TAR"],
+}
+extention_list = [
+    "JPEG",
+    "PNG",
+    "JPG",
+    "SVG",
+    "DOC",
+    "DOCX",
+    "TXT",
+    "PDF",
+    "XLSX",
+    "PPTX",
+    "AVI",
+    "MP4",
+    "MOV",
+    "MKV",
+    "MP3",
+    "OGG",
+    "WAV",
+    "AMR",
+    "ZIP",
+    "GZ",
+    "TAR",
+]
+# Sets for known and unknown extentions
 known_ext = set()
 unknown_ext = set()
 
-# print(normalize("Выхожу   odin (я) на дорогу&*!2.mp3"))
-work_list = os.listdir(source_folder)
-print(work_list)
-# for w in work_list:
-#     print(w)
-
-
-# os.rename(src, dst, *, src_dir_fd=None, dst_dir_fd=None)
-#  - переименовывает файл или директорию из src в ds
-# os.listdir(path=".") - список файлов и директорий в папке.
-# os.chdir(path) - смена текущей директории.
-# os.mkdir(path, mode=0o777, *, dir_fd=None) - создаёт директорию. OSError, если директория существует.
-# os.rmdir(path, *, dir_fd=None) - удаляет пустую директорию.
-# os.system(command) -
-# исполняет системную команду, возвращает код её завершения (в случае успеха 0)
-# os.path.isdir(path) - проверка является ли путь директорией.
-# os.path.isfile(path) - является ли путь файлом.
-
-# if result.is
-
+make_dir()  # Создаем папки назначения
+move_files()  # Переносим файлы известных типов в папки назначения
+remove_empty_folders()  # Удаляем пустые папки
+find_unknown_ext()  # Собираем неизвестные расширения
+rename_all_files()  # Переводим названия фалов в транслит
+unpack_archives()  # Распаковываем архивы
